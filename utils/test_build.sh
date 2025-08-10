49daa0673536b3898935345d89faa235102dda46
Denis von Arx <dvarx@ethz.ch>
2025-08-10 15:48:36 +0200

    adjusted image / readme
    adjusted cicd
    
    adjusted cicd
    
    adjusted dockerfile & buildscript

--- utils/test_build.sh
+++ utils/test_build.sh
@@ -2,9 +2,9 @@
 
 #clone the source code
 cd src
-git clone https://github.com/dvarx/mdriver_fw.git
-cd mdriver_fw
+git clone https://github.com/$REPO_NAME.git -b $BRANCH_NAME
 
+REPO_NAME_SHORT="${REPO_NAME#*/}"
 CCS_DIR=/home/ubuntu/ti/ccs
 C2000WareDir=/home/ubuntu/ti/C2000Ware_3_04_00_00
 SRC_DIR=/home/ubuntu/src
@@ -13,9 +13,9 @@ export SRC_DIR
 export C2000WareDir
 
 #build the CPU1 code
-cd $HOME/src/mdriver_fw/mdriver_cpu1/build_dir
+cd $HOME/src/$REPO_NAME_SHORT/mdriver_cpu1/build_dir
 $CCS_DIR/utils/bin/gmake -k -j 14 all -O
 
 #build the CM code
-cd $HOME/src/mdriver_fw/mdriver_cm/build_dir
+cd $HOME/src/$REPO_NAME_SHORT/mdriver_cm/build_dir
 $CCS_DIR/utils/bin/gmake -k all
